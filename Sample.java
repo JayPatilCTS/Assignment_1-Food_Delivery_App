65fdd69f4de1184a27b5b05da939ddb67083210e
2464331 <VishalShirish.Lalge@cognizant.com>
2025-12-24 10:19:03 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,11 @@
+package basicSyntax;
+
+public class Sample {
+
+    public static void main(String args[]){
+        int a = 12;
+        int b = 34;
+        int addition = a+b;
+        System.out.println(addition);
+    }
+}
